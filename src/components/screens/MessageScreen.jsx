1272d96b36b38479aaf846afc42f3e11b09658c0
hawkeyerj <122152933+hawkeyerj@users.noreply.github.com>
2026-07-18 15:47:20 +0530

    Update message content in MessageScreen component

--- src/components/screens/MessageScreen.jsx
+++ src/components/screens/MessageScreen.jsx
@@ -3,8 +3,8 @@ import { motion, AnimatePresence } from "framer-motion"
 import { Heart, MoveRight } from "lucide-react"
 
 const message = `
-
-`Meri jaan tu meri jindagi ka sabse khoobsurat hissa he . Tere saath ek choti si baat , ek chota sa moment bhi bahut special lgta he aur . Morning call pr teri ek pyaari si smile mere pure din ko sundar bnaa deti he .Tu sirf meri gf hi nhi meri sabse pyaari aadat ban chuki he . Jiss aadat ko me kabhi nhi chodhna chahunga . I love you and I care for you a lot my kaddu . Me bass yahi chahta hu ki tu humesha khush rahe 💋 . Because our smile makes me happy I never want you to loose it 💋. Love you always ❤️🧿.
+Meri jaan tu meri jindagi ka sabse khoobsurat hissa he . Tere saath ek choti si baat , ek chota sa moment bhi bahut special lgta he aur . Morning call pr teri ek pyaari si smile mere pure din ko sundar bnaa deti he .Tu sirf meri gf hi nhi meri sabse pyaari aadat ban chuki he . Jiss aadat ko me kabhi nhi chodhna chahunga . I love you and I care for you a lot my kaddu . Me bass yahi chahta hu ki tu humesha khush rahe 💋 . Because our smile makes me happy I never want you to loose it 💋. Love you always ❤️🧿. 
+`
 
 export default function MessageScreen({ onNext }) {
     const [isOpen, setIsOpen] = useState(false)
